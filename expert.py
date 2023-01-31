2eb58de54264d99d3a0d16d0faf5a9511040b856
punishedchumbucket6000 <nathanieljamesallen2@gmail.com>
2023-01-31 17:28:34 -0500

    Sports ID

--- expert.py
+++ expert.py
@@ -40,6 +40,47 @@ class Expert:
         print(f'\n{self.dialogue["farewell"]}')
         if has_contributed:
             print(self.dialogue["contribution"])
+    
+    def __init__(self, kb_path, dialogue_path):
+        self.kb_path = kb_path
+        self.dialogue_path = dialogue_path
+        
+        with open(self.kb_path) as kb_file:
+            self.kb = json.load(kb_file)
+        with open(self.dialogue_path) as d_file:
+            self.dialogue = json.load(d_file)
+
+    def start(self):
+        keep_going = True
+        has_contributed = False
+        print(self.dialogue["intro"] )
+
+        while keep_going:
+            r1 = input(f'{self.dialogue["q1"]}\n').lower()
+            r2 = 1 if input(f'\n{self.dialogue["q2"]}\n').lower()[0] == "y" else 0
+
+            # create key from input
+            sports_key = f"{r1}-{r2}"
+
+            # get fruit dictionary from key
+            sports_value = self.kb.get(sports_key)
+
+            # if the key exists print the fruit name
+            if sports_value: 
+                print(f'\n{self.dialogue["conclusion"]} {sports_value.get("name")}.\n')
+            else:
+                new_sport = input(f'\n{self.dialogue["not_found"]}\n').lower()
+                self.kb[sports_key] = {"name" : new_sport}
+                with open(self.kb_path, 'w') as kb_file:
+                    json.dump(self.kb, kb_file)
+                has_contributed = True
+                
+
+            keep_going = input(f'{self.dialogue["repeat"]}\n').upper()[0] == "Y"
+
+        print(f'\n{self.dialogue["farewell"]}')
+        if has_contributed:
+            print(self.dialogue["contribution"])
 
 
 def main():
--- main.py
+++ main.py
@@ -4,6 +4,10 @@ def main():
     print("Testing, I'm in main.")
     e1 = Expert('data/kb_fruit.json','data/dialogue_fruit.json')
     e1.start()
+    e2 = Expert('data/kb_sports.json','data/dialogue_sports.json')
+    e2.start()
+
+    
 
 if __name__ == "__main__":
     main()
